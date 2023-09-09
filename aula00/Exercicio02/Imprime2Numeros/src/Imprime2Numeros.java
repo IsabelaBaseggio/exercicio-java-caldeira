5ebb8ae66f700069f29124a9336d35bcc62f9f2e
IsabelaBaseggio <isabela.baseggio7@gmail.com>
2023-09-08 22:46:41 -0300

    Feature branch criada e execício 2 feito

---
+++ aula00/Exercicio02/Imprime2Numeros/src/Imprime2Numeros.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Imprime2Numeros {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o primero número: ");
+        int primeiroNumero = scan.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int segundoNumero = scan.nextInt();
+
+        System.out.println("Os número digitados são " + primeiroNumero + " e " + segundoNumero);
+    }
+}
