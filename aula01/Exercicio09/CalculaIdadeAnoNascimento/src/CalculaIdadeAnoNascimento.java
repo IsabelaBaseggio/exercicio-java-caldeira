daf0381b0b99a46153be5c17ca7e625a253315e4
IsabelaBaseggio <isabela.baseggio7@gmail.com>
2023-09-10 02:48:04 -0300

    Exercício 9 feito

---
+++ aula01/Exercicio09/CalculaIdadeAnoNascimento/src/CalculaIdadeAnoNascimento.java
@@ -0,0 +1,28 @@
+import java.util.Calendar;
+import java.util.Scanner;
+import java.util.TimeZone;
+
+public class CalculaIdadeAnoNascimento {
+    public static void main(String[] args) {
+        int idade = 0;
+
+        Calendar calendario = Calendar.getInstance(TimeZone.getTimeZone("Brazil/East"));
+        int anoAtual = calendario.get(Calendar.YEAR);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite o seu ano de nascimento: ");
+        int anoDeNacimento = scan.nextInt();
+
+        if (anoDeNacimento < 0 || anoDeNacimento > anoAtual){
+            while (anoDeNacimento < 0 || anoDeNacimento > anoAtual) {
+                System.out.println("Ano inválido. Por favor, digite novamente.");
+                System.out.print("Ano de nascimento: ");
+                anoDeNacimento = scan.nextInt();
+            }
+        }
+
+        idade = anoAtual - anoDeNacimento;
+
+        System.out.println("A sua idade é " + idade);
+    }
+}
