19a5d5a4b75381d09f1dd2535e39bdbc4bd0fc80
IsabelaBaseggio <isabela.baseggio7@gmail.com>
2023-09-25 18:09:10 -0300

    aula 5: exercício 1 feito - completo

---
+++ aula05/Exercicio01/Array5Numeros/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MediaNumeros media5Numeros = new MediaNumeros();
+        media5Numeros.iserirNumeros();
+
+        System.out.println(media5Numeros.calcularMedia());
+
+    }
+}
