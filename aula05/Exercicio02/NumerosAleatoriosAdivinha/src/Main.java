c0e4b1faef561354e95fb95668a48f50b21c909e
IsabelaBaseggio <isabela.baseggio7@gmail.com>
2023-09-25 18:35:51 -0300

    aula 5: exercício 2 feito - completo

---
+++ aula05/Exercicio02/NumerosAleatoriosAdivinha/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        NumerosAleatorios numerosAleatorios = new NumerosAleatorios();
+
+        System.out.println(numerosAleatorios.procurarNumero(8));
+    }
+}
