02cda9092b0b1db2f252910ae154ba6eb177ad32
IsabelaBaseggio <isabela.baseggio7@gmail.com>
2023-09-25 23:21:28 -0300

    aula 5: exercício 3 completo - refatorado

--- aula05/Exercicio03/ListaDeTarefas/src/ListaDeTarefas.java
+++ aula05/Exercicio03/ListaDeTarefas/src/ListaDeTarefas.java
@@ -26,17 +26,17 @@ public class ListaDeTarefas {
         for (int i = 0; i < listaDeTarefas.size(); i++) {
             System.out.println("[" + (i+1) + "] " + listaDeTarefas.get(i).getData() + " - " + listaDeTarefas.get(i).getTitulo() + ": " + listaDeTarefas.get(i).getDescricao());
         }
-        System.out.println("[" + listaDeTarefas.size() + "] - CANCELAR");
+        System.out.println("[" + (listaDeTarefas.size()+1) + "] - CANCELAR");
         System.out.println("Informe o número da tarefa que você deseja excluir ou a opção de cancelar.");
         System.out.print("Opção: ");
         int opcao = scan.nextInt();
 
-        while (opcao < 1 || opcao > listaDeTarefas.size()) {
+        while (opcao < 1 || opcao > (listaDeTarefas.size() +1 )) {
             System.out.print("Informe uma opção válida: ");
             opcao = scan.nextInt();
         }
 
-        if (opcao < listaDeTarefas.size()) {
+        if (opcao < (listaDeTarefas.size() + 1)) {
             listaDeTarefas.remove(opcao - 1);
             System.out.println("Tarefa excluída com sucesso!");
         } else {
