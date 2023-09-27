38172cc3ee3fd82d8ad3de06c51a07654aafd105
IsabelaBaseggio <isabela.baseggio7@gmail.com>
2023-09-27 00:24:17 -0300

    aula 5: exercício 6 feito - completo

--- aula05/Exercicio06/ListaDeReservas/src/Main.java
+++ aula05/Exercicio06/ListaDeReservas/src/Main.java
@@ -1,12 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Exercício 6");
-        String data = "01234567";
-        data = data.replaceAll("/", "");
-        System.out.println(data);
+        ListaDeReservas listaDeReservas = new ListaDeReservas();
 
-        String novaString = data.substring(4,8);
+        listaDeReservas.reservarPassagem("Isabela", "32601178221", "Rio de Janeiro - RJ", 2);
 
-        System.out.println(novaString);
+        System.out.println(" ");
+
+        listaDeReservas.reservarPassagem("Isabela", "32601178221", "Rio de Janeiro - RJ", 2);
+
+        System.out.println(" ");
+
+        listaDeReservas.reservarPassagem("Isabela", "00000000000", "Rio de Janeiro - RJ", 2);
+
+        System.out.println(" ");
+
+        listaDeReservas.reservarPassagem("Rafael", "32601178221", "São Paulo - SP", 2);
+
+        System.out.println(" ");
+
+        listaDeReservas.listarReservas();
+
+        System.out.println(" ");
+
+        listaDeReservas.alterarDataDeReserva("04871216004");
+
+        System.out.println(" ");
+
+        listaDeReservas.alterarLocalDaPassagem("04871216004");
+
+        System.out.println(" ");
+
+        listaDeReservas.alterarQuantidadeDePassagem("04871216004");
+
+        System.out.println(" ");
+
+        listaDeReservas.listarReservas();
+
+        System.out.println(" ");
+
+        listaDeReservas.cancelarReserva("04871216004");
+
+
+
     }
 }
