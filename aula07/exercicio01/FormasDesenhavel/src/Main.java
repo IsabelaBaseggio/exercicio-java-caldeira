309b20ebb1e0559e55b0430633a2098b2784562f
IsabelaBaseggio <isabela.baseggio7@gmail.com>
2023-10-02 23:31:58 -0300

    aula 7: exercício 1 feito - completo

---
+++ aula07/exercicio01/FormasDesenhavel/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo();
+        System.out.println(circulo.desenhar());
+
+        Quadrado quadrado = new Quadrado();
+        System.out.println(quadrado.desenhar());
+    }
+}
