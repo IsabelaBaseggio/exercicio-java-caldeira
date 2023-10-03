60de7d49e3a8fe2cd8f209060b2babef3c9eca95
IsabelaBaseggio <isabela.baseggio7@gmail.com>
2023-10-03 16:44:08 -0300

    Aula 7: Exercício 4 - classe CalculaInteiros falta usar enum OperacaoMatematica

---
+++ aula07/exercicio04/CalculaNumerosInteiros/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        CalculaInteiros calcula = new CalculaInteiros();
+
+        System.out.println(calcula.escolherOperacao());
+    }
+}
