6188e61b11a52574087d7c83baa1707e7c363129
IsabelaBaseggio <isabela.baseggio7@gmail.com>
2023-10-03 15:31:25 -0300

    Aula 7: Exercício 2 feito - completo

---
+++ aula07/exercicio02/DiaDaSemana/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        BuscaDiaDaSemana diaDaSemana = new BuscaDiaDaSemana();
+        diaDaSemana.buscarMensagemDoDia(4);
+    }
+}
